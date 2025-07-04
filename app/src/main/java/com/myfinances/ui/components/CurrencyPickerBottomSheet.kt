3ebe70ecc8d2878f78df9a2e525643c98743a39e
krishigina <krishigina@gmail.com>
2025-07-05 00:32:21 +0300

    done: HW4

--- app/src/main/java/com/myfinances/ui/components/CurrencyPickerBottomSheet.kt
+++ app/src/main/java/com/myfinances/ui/components/CurrencyPickerBottomSheet.kt
@@ -13,8 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -25,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.myfinances.R
@@ -91,7 +90,7 @@ fun CurrencyPickerBottomSheet(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Icon(
-                        imageVector = Icons.Default.Close,
+                        painter = painterResource(R.drawable.ic_account_exit),
                         contentDescription = stringResource(R.string.action_cancel),
                         tint = Color.White
                     )
